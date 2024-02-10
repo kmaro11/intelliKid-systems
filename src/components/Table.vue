840b6eb16b5323b373383db7083234d81257a3fb
Rokas <rokas.kmaro@gmail.com>
2024-02-10 14:36:45 +0200

    added virtual list

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,15 +1,16 @@
 <script lang="ts" setup>
-import {ref} from 'vue'
+import {onMounted, ref} from 'vue'
 import {useFetchData} from "../composables/useFetchData.ts";
 import {useListSort} from "../composables/useListSort.ts";
+import {useVirtualList} from "@vueuse/core";
 
 const tableColumns = ref<{ label: string, key: number }[]>([])
-const tableRows = ref<string[]>([])
+const tableRows = ref<Record<string, string>[]>([])
 
 const fetchData = async (): Promise<void> => {
   const data: {
     columns: string[],
-    rows: string[]
+    rows: Record<string, string>[]
   } = await useFetchData('./src/data/huge.json');
 
   if (data) {
@@ -24,48 +25,78 @@ const fetchData = async (): Promise<void> => {
   }
 }
 
-fetchData()
+const getRowCellText = (row: Record<string, string>, column: string): string => {
+  return row[column]
+}
+
+const {list, containerProps, wrapperProps} = useVirtualList(
+    tableRows,
+    {
+      itemHeight: 40,
+    }
+);
+
+onMounted(() => fetchData())
 </script>
 <template>
-  <div>
-
-    <table>
-      <tr>
-        <th
-          v-for="column in tableColumns"
-          :key="column.key"
+  <div class="table">
+    <div v-bind="containerProps" style="max-height: 90vh; height: 100%" >
+      <div class="table-row--header table-row">
+        <div
+            v-for="column in tableColumns"
+            :key="column.key"
+            class="table-cell"
         >
           {{ column.label }}
-        </th>
-      </tr>
-      <tr
-        v-for="row in tableRows"
-        :key="row"
-      >
-        <td
-          v-for="column in tableColumns"
-          :key="column.key"
-        >
-          {{ row[column.label] }}
-        </td>
-      </tr>
-    </table>
+        </div>
+      </div>
 
+      <div v-bind="wrapperProps">
+        <div
+            v-for="{ index, data } in list"
+            :key="index"
+            class="table-row"
+        >
+          <div
+              v-for="column in tableColumns"
+              :key="column.key"
+              class="table-cell"
+          >
+            {{ getRowCellText(data, column.label) }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+
 </template>
 <style scoped lang="css">
-
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
+.table{
+  --main-grey-color: #ececec;
 }
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
+.table-row {
+  display: flex;
+}
+
+.table-cell {
+  width: 80px;
   padding: 8px;
+  flex-shrink: 0;
+  border-right: 1px solid var(--main-grey-color);
+  border-bottom: 1px solid var(--main-grey-color);
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 
 
+.table-row--header .table-cell {
+  font-weight: 700;
+  border-top: 1px solid var(--main-grey-color);
+}
+
+.table-row .table-cell:last-of-type {
+  border-right: none;
+}
 </style>
